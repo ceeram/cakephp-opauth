eb531ff130b848ae3b4c9c03d8fe6a9c66589bd6
Ceeram <c33ram@gmail.com>
2013-07-13 14:23:01 +0200

    fix routes if no path configured

--- Config/routes.php
+++ Config/routes.php
@@ -1,3 +1,6 @@
 <?php
 $path = Configure::read('Opauth.path');
+if (!$path) {
+	$path = '/auth/';
+}
 Router::connect($path . '*', array('plugin' => 'opauth', 'controller' => 'opauth', 'action' => 'login'));
